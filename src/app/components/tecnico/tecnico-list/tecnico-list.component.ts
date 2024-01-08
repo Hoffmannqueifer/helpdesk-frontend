81f2f14a2cef13e5b56fdc00854c0e250bc75086
hoffmannqueifer <hoffmannqueifer23@gmail.com>
2024-01-08 14:12:51 -0300

    Criando o componente Tecnico-list, add na rota filha e realizando o ajuste do Html e CSS da tela

---
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
+
+@Component({
+  selector: 'app-tecnico-list',
+  templateUrl: './tecnico-list.component.html',
+  styleUrls: ['./tecnico-list.component.css']
+})
+export class TecnicoListComponent {
+  ELEMENT_DATA: Tecnico[] = [{
+    id: 1,
+    nome: 'hoffmann queifer',
+    cpf: '123.345.456-10',
+    email: 'hoffmannqueifer@gmail.com',
+    senha: '12345',
+    perfis: ['0'],
+    dataCriacao: '08/01/2024'
+  }]
+
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+}
+
+
