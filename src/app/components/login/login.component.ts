f5bfb661753f0104f590d2295d18f3fde2212869
hoffmannqueifer <hoffmannqueifer23@gmail.com>
2024-01-08 22:20:16 -0300

    add serviço de mensagens toastr para o usuario

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
 
 @Component({
@@ -16,6 +17,15 @@ export class LoginComponent {
 
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(8));
+
+  constructor(private toast: ToastrService){
+
+  }
+
+  logar(){
+    this.toast.error('Usuário e/ou senha invalidos!!', 'Login');
+    this.creds.senha = '';
+  }
 
   validaCampos(): boolean{
     if(this.email.valid && this.senha.valid){
